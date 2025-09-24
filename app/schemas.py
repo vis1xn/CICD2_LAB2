e5d88e3740c0fb44d01a8ef3962deb92f973d5d9
Tyrese <g00421432@atu.ie>
2025-09-24 12:39:12 +0100

    adding helath, student id and delete

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@
 from pydantic import BaseModel, EmailStr, constr, conint
 class User(BaseModel):
  user_id: int
+ student_id: constr(regex='^S.*$')
  name: constr(min_length=2, max_length=50)
  email: EmailStr
  age: conint(gt=18)
